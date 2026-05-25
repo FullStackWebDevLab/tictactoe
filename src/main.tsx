beddc5f4a69c8161e0a309ba4ca3587e3f503802
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-05-25 18:17:18 +0300

    Add vercel analytics.

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { Analytics } from "@vercel/analytics/next" // for vercel analytics
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
